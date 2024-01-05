de96ed8ca586c48cd7edd531a1b9a33440bf3017
yayat_99 <rahmat999ht@gmail.com>
2024-01-05 15:51:08 +0800

    update navbar

---
+++ src/app/items/customer.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import React from "react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  User,
+  Chip,
+  Tooltip,
+  // type ChipProps,
+  // getKeyValue,
+} from "@nextui-org/react";
+import { type ChipProps } from "@nextui-org/react";
+import { EditIcon } from "public/icons/EditIcon";
+import { DeleteIcon } from "public/icons/DeleteIcon";
+import { EyeIcon } from "public/icons/EyeIcon";
+import { columns, users } from "public/data";
+
+const statusColorMap: Record<string, ChipProps["color"]> = {
+  active: "success",
+  paused: "danger",
+  vacation: "warning",
+};
+
+type User = (typeof users)[0];
+
+export default function Customer() {
+  const renderCell = React.useCallback((user: User, columnKey: React.Key) => {
+    const cellValue = user[columnKey as keyof User];
+
+    switch (columnKey) {
+      case "name":
+        return (
+          <User
+            avatarProps={{ radius: "lg", src: user.avatar }}
+            description={user.email}
+            name={cellValue}
+          >
+            {user.email}
+          </User>
+        );
+      case "role":
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-sm capitalize">{cellValue}</p>
+            <p className="text-bold text-sm capitalize text-default-400">
+              {user.team}
+            </p>
+          </div>
+        );
+      case "status":
+        return (
+          <Chip
+            className="capitalize"
+            color={statusColorMap[user.status]}
+            size="sm"
+            variant="flat"
+          >
+            {cellValue}
+          </Chip>
+        );
+      case "actions":
+        return (
+          <div className="relative flex items-center gap-2">
+            <Tooltip content="Details">
+              <span className="cursor-pointer text-lg text-default-400 active:opacity-50">
+                <EyeIcon />
+              </span>
+            </Tooltip>
+            <Tooltip content="Edit user">
+              <span className="cursor-pointer text-lg text-default-400 active:opacity-50">
+                <EditIcon />
+              </span>
+            </Tooltip>
+            <Tooltip color="danger" content="Delete user">
+              <span className="cursor-pointer text-lg text-danger active:opacity-50">
+                <DeleteIcon />
+              </span>
+            </Tooltip>
+          </div>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
+
+  return (
+    <Table aria-label="Example table with custom cells">
+      <TableHeader columns={columns}>
+        {(column) => (
+          <TableColumn
+            key={column.uid}
+            align={column.uid === "actions" ? "center" : "start"}
+          >
+            {column.name}
+          </TableColumn>
+        )}
+      </TableHeader>
+      <TableBody items={users}>
+        {(item) => (
+          <TableRow key={item.id}>
+            {(columnKey) => (
+              <TableCell>{renderCell(item, columnKey)}</TableCell>
+            )}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,12 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
+import { nextui } from "@nextui-org/react";
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./src/**/*.tsx",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -10,5 +14,55 @@ export default {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
+  // plugins: [
+  //   nextui({
+  //     themes: {
+  //       light: {
+  //         layout: {}, // light theme layout tokens
+  //         colors: {}, // light theme colors
+  //       },
+  //       dark: {
+  //         layout: {}, // dark theme layout tokens
+  //         colors: {}, // dark theme colors
+  //       },
+  //       modern: {
+  //         extend: "dark", // <- inherit default values from dark theme
+  //         colors: {
+  //           background: "#0D001A",
+  //           foreground: "#ffffff",
+  //           primary: {
+  //             50: "#3B096C",
+  //             100: "#520F83",
+  //             200: "#7318A2",
+  //             300: "#9823C2",
+  //             400: "#c031e2",
+  //             500: "#DD62ED",
+  //             600: "#F182F6",
+  //             700: "#FCADF9",
+  //             800: "#FDD5F9",
+  //             900: "#FEECFE",
+  //             DEFAULT: "#DD62ED",
+  //             foreground: "#ffffff",
+  //           },
+  //           focus: "#F182F6",
+  //         },
+  //         layout: {
+  //           disabledOpacity: "0.3",
+  //           radius: {
+  //             small: "1px",
+  //             medium: "2px",
+  //             large: "4px",
+  //           },
+  //           borderWidth: {
+  //             small: "1px",
+  //             medium: "2px",
+  //             large: "3px",
+  //           },
+  //         },
+  //       },
+  //     },
+  //   }),
+  // ],
 } satisfies Config;
